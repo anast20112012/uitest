5b6cc31f4bca158c68db92e9d10826231c606cb3
nke <nke>
2024-09-27 08:54:46 +0700

    Initial Commit

---
+++ UITestingDemo/UITestingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITestingDemoApp.swift
+//  UITestingDemo
+//
+//  Created by user on 27.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct UITestingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
